917cdd388de262541894b5e63f61f9ede5e8d11c
diegomontanari <diegomontanari.imp@gmail.com>
2025-03-08 16:41:17 +0100

    Update Ex4.c

--- Es1/Ex4.c
+++ Es1/Ex4.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-int	main()
+int main()
 {
-	int	num;
-	printf("Inserisci un numero");
-	scanf("%d", &num);
-	for (int i = 0; i <= num - 1; i++) // for per righe
-	{
-		for (int j = 0; j <= num - 1; j++) // for per singola riga
-		{
-			
-			if ( j == 0 || j == num -1)
-				printf("*");
-			else
-				printf(" ");
-		}
-		printf("\n");
-	}
-	return 0;
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for (int i = 0; i <= num - 1; i++) // Loop for rows
+    {
+        if (i == 0 || i == num - 1) // Print first and last row with all "*"
+        {
+            for (int cont = 0; cont < num; cont++) // Loop to print "*"
+                printf("*");
+            printf("\n"); // Add newline after printing the row of "*"
+        }
+        else
+        {
+            for (int j = 0; j <= num - 1; j++) // Loop for single row
+            {
+                if (j == 0 || j == num - 1)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
 }
